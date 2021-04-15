48305d9b82a5074fe57c541a5ac14481f6fcd78a
mrjarvis698 <dudamajaykumar59@gmail.com>
2021-04-15 19:59:07 +0000

    miatoll: Build Octavi

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,13 +1,13 @@
 #
-# Copyright (C) 2020 The LineageOS Project
+# Copyright (C) 2020 The octaviOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_miatoll.mk
+    $(LOCAL_DIR)/octavi_miatoll.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_miatoll-user \
-    lineage_miatoll-userdebug \
-    lineage_miatoll-eng
+    octavi_miatoll-user \
+    octavi_miatoll-userdebug \
+    octavi_miatoll-eng
--- octavi_miatoll.mk
+++ octavi_miatoll.mk
@@ -13,16 +13,19 @@
 # limitations under the License.
 
 $(call inherit-product, device/xiaomi/miatoll/device.mk)
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/octavi/config/common_full_phone.mk)
 
 # Bootanimation Resolution
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_miatoll
+PRODUCT_NAME := octavi_miatoll
 PRODUCT_DEVICE := miatoll
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := SM6250
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
+
+# Octavi Official & Maintainer
+OCTAVI_BUILD_TYPE=Official
+OCTAVI_DEVICE_MAINTAINER := mrjarvis698
